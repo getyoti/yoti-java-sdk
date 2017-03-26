b977f1df79628ca532f29ffcd145f5016987bbed
davidgoate <david.goate@gmail.com>
2017-03-26 19:43:13 +0100

    Provided two system properties to allow the client HTTP connection timeout behaviour to be configured.
    * `yoti.client.connect.timeout.ms` - the number of milliseconds that you are prepared to wait for the connection to be established. Zero is interpreted as an infinite timeout.
    * `yoti.client.read.timeout.ms` - the number of milliseconds that you are prepared to wait for data to become available to read in the response stream. Zero is interpreted as an infinite timeout.
    
    Also updated the read me docs to reflect this change and remove reference to activityDetails.getOutcome() method this this appears to have been removed.

--- java-sdk-impl/src/main/java/com/yoti/api/client/spi/remote/call/UrlConnector.java
+++ java-sdk-impl/src/main/java/com/yoti/api/client/spi/remote/call/UrlConnector.java
@@ -5,21 +5,43 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class UrlConnector {
-    private final String urlString;
 
-    private UrlConnector(String urlString) {
+    private static final String DEFAULT_YOTI_CONNECT_TIMEOUT = "0";
+    private static final String DEFAULT_YOTI_READ_TIMEOUT = "0";
+    private static final String PROPERTY_YOTI_CONNECT_TIMEOUT = "yoti.client.connect.timeout.ms";
+    private static final String PROPERTY_YOTI_READ_TIMEOUT = "yoti.client.read.timeout.ms";
+
+    private final String urlString;
+    private final int connectTimeout;
+    private final int readTimeout;
+
+    private UrlConnector(final String urlString) {
         this.urlString = urlString;
+        this.connectTimeout = getPropertyOrDefaultAsInt(PROPERTY_YOTI_CONNECT_TIMEOUT, DEFAULT_YOTI_CONNECT_TIMEOUT);
+        this.readTimeout = getPropertyOrDefaultAsInt(PROPERTY_YOTI_READ_TIMEOUT, DEFAULT_YOTI_READ_TIMEOUT);
     }
 
-    public static UrlConnector get(String urlString) {
+    public static UrlConnector get(final String urlString) {
         return new UrlConnector(urlString);
     }
 
     public HttpURLConnection getHttpUrlConnection() throws IOException {
-        return (HttpURLConnection) new URL(urlString).openConnection();
+        final HttpURLConnection connection = (HttpURLConnection) new URL(urlString).openConnection();
+        configureTimeouts(connection);
+        return connection;
     }
 
     String getUrlString() {
         return urlString;
+    }
+
+    private void configureTimeouts(final HttpURLConnection connection) {
+        connection.setConnectTimeout(connectTimeout);
+        connection.setReadTimeout(readTimeout);
+    }
+
+    private int getPropertyOrDefaultAsInt(final String propertyKey, final String defaultValue) {
+        final String value = System.getProperty(propertyKey, defaultValue);
+        return Integer.parseInt(value);
     }
 }
