ce787b77f7a52b07c5cd0872389969bcd0fee7bd
davidgoate <david.goate@gmail.com>
2017-06-08 18:27:31 +0100

    Documentation update to make it more clear that tokens are single use only.

--- java-sdk-api/src/main/java/com/yoti/api/client/YotiClient.java
+++ java-sdk-api/src/main/java/com/yoti/api/client/YotiClient.java
@@ -15,8 +15,10 @@ public interface YotiClient {
      * Get the activity details for a token. Amongst others contains the user profile with the user's attributes you
      * have selected in your application configuration on Yoti Portal.
      *
+     * <b>Note: encrypted tokens should only be used once. You should not invoke this method multiple times with the same token.</b>
+     *
      * @param encryptedYotiToken
-     *            encrypted Yoti token (can be only decrypted with your application's private key)
+     *            encrypted Yoti token (can be only decrypted with your application's private key). Note that this token must only be used once.
      * @return an {@link ActivityDetails} instance holding the user's attributes
      *
      * @throws ProfileException
