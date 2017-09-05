a840380d8f7c87809b65f18d1c8e68fea96fa015
davidgoate <david.goate@gmail.com>
2017-09-05 17:37:51 +0100

    Reverted commit 37825d3bbd9664ba9ec823253d6eb025bc289142 (merged conflicts).

---
+++ java-sdk-springboot-example/src/main/java/com/yoti/api/examples/springboot/YotiLoginController.java
@@ -0,0 +1,52 @@
+package com.yoti.api.examples.springboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.yoti.api.client.ActivityDetails;
+import com.yoti.api.client.HumanProfile;
+import com.yoti.api.client.ProfileException;
+import com.yoti.api.client.YotiClient;
+
+@Controller
+public class YotiLoginController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(YotiLoginController.class);
+
+    private final YotiClient client;
+
+    @Autowired
+    public YotiLoginController(final YotiClient client) {
+        this.client = client;
+    }
+
+    /**
+     * This endpoint is the "Callback URL" which will be called by user's browser after user logs in. It's a GET endpoint.
+     * We will pass you a token inside url query string (/login?token=token-value)
+     */
+    @RequestMapping("/login")
+    public String doLogin(@RequestParam("token") final String token, final Model model) {
+        ActivityDetails activityDetails;
+        HumanProfile profile;
+        try {
+            activityDetails = client.getActivityDetails(token);
+            profile = activityDetails.getUserProfile();
+        } catch (final ProfileException profileException) {
+            LOG.info("Could not get profile", profileException);
+            return "error";
+        }
+
+        // load profile data into model
+        model.addAttribute("name", profile.getGivenNames());
+        model.addAttribute("phone", profile.getPhoneNumber());
+        model.addAttribute("userId", activityDetails.getUserId());
+
+        return "home";
+    }
+
+}
