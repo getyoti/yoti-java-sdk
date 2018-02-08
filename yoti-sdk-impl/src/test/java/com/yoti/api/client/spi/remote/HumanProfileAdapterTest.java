b7ee6ac90f6f4a9643c3015d05644d5a9e9f9745
Michael Buck <michael.buck@yoti.com>
2018-02-08 11:18:32 +0000

    API-357: First tidy up HumanProfileAdapterTest

--- yoti-sdk-impl/src/test/java/com/yoti/api/client/spi/remote/HumanProfileAdapterTest.java
+++ yoti-sdk-impl/src/test/java/com/yoti/api/client/spi/remote/HumanProfileAdapterTest.java
@@ -1,16 +1,23 @@
 package com.yoti.api.client.spi.remote;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
 import com.yoti.api.client.DocumentDetails;
 import com.yoti.api.client.HumanProfile;
 import com.yoti.api.client.Profile;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
+@RunWith(MockitoJUnitRunner.class)
 public class HumanProfileAdapterTest {
+
     private static final String GIVEN_NAMES = "Given Names";
     private static final String GENDER_MALE = "MALE";
     private static final String GENDER_INVALID = "X";
@@ -18,85 +25,103 @@ public class HumanProfileAdapterTest {
     private static final String INVALID_DOCUMENT_DETAILS = "invalid";
     private static final String FAMILY_NAME = "Family Name";
 
-    @Test
-    public void shouldReturnGender() {
-        Profile profile = mock(Profile.class);
-        when(profile.getAttribute("gender")).thenReturn(GENDER_MALE);
-        HumanProfile p = HumanProfileAdapter.wrap(profile);
-        assertEquals(HumanProfile.Gender.MALE, p.getGender());
+    HumanProfileAdapter testObj;
+
+    @Mock Profile profileMock;
+
+    @Before
+    public void setUp() throws Exception {
+        testObj = (HumanProfileAdapter) HumanProfileAdapter.wrap(profileMock);
     }
 
     @Test
-    public void shouldNotReturnGenderForNullAttribute() {
-        Profile profile = mock(Profile.class);
-        HumanProfile p = HumanProfileAdapter.wrap(profile);
-        assertNull(p.getGender());
+    public void getGender_shouldReturnGender() {
+        when(profileMock.getAttribute("gender")).thenReturn(GENDER_MALE);
+
+        HumanProfile.Gender result = testObj.getGender();
+
+        assertEquals(HumanProfile.Gender.MALE, result);
     }
 
     @Test
-    public void shouldNotReturnGenderForInvalidAttribute() {
-        Profile profile = mock(Profile.class);
-        when(profile.getAttribute("gender")).thenReturn(GENDER_INVALID);
-        HumanProfile p = HumanProfileAdapter.wrap(profile);
-        assertEquals(HumanProfile.Gender.OTHER, p.getGender());
+    public void getGender_shouldNotReturnGenderForNullAttribute() {
+        HumanProfile.Gender result = testObj.getGender();
+
+        assertNull(result);
     }
 
     @Test
-    public void shouldReturnDocumentDetails() {
-        Profile profile = mock(Profile.class);
-        when(profile.getAttribute("document_details")).thenReturn(VALID_DOCUMENT_DETAILS);
-        HumanProfile p = HumanProfileAdapter.wrap(profile);
-        assertEquals(DocumentDetails.DocumentType.PASSPORT, p.getDocumentDetails().getType());
+    public void getGender_shouldNotReturnGenderForInvalidAttribute() {
+        when(profileMock.getAttribute("gender")).thenReturn(GENDER_INVALID);
+
+        HumanProfile.Gender result = testObj.getGender();
+
+        assertEquals(HumanProfile.Gender.OTHER, result);
     }
 
     @Test
-    public void shouldNotReturnDocumentDetailsForNullAttribute() {
-        Profile profile = mock(Profile.class);
-        HumanProfile p = HumanProfileAdapter.wrap(profile);
-        assertNull(p.getDocumentDetails());
+    public void getDocumentDetails_shouldReturnDocumentDetails() {
+        when(profileMock.getAttribute("document_details")).thenReturn(VALID_DOCUMENT_DETAILS);
+
+        DocumentDetails result = testObj.getDocumentDetails();
+
+        assertEquals(DocumentDetails.DocumentType.PASSPORT, result.getType());
     }
 
     @Test
-    public void shouldNotReturnDocumentDetailsForInvalidAttribute() {
-        Profile profile = mock(Profile.class);
-        when(profile.getAttribute("document_details")).thenReturn(INVALID_DOCUMENT_DETAILS);
-        HumanProfile p = HumanProfileAdapter.wrap(profile);
-        assertNull(p.getDocumentDetails());
+    public void getDocumentDetails_shouldNotReturnDocumentDetailsForNullAttribute() {
+        DocumentDetails result = testObj.getDocumentDetails();
+
+        assertNull(result);
     }
 
     @Test
-    public void getGivenAndLastNamesShouldReturnNullWhenOtherAttributesAreBothNull() {
-        Profile profile = mock(Profile.class);
-        when(profile.getAttribute("given_names")).thenReturn(null);
-        when(profile.getAttribute("family_name")).thenReturn(null);
-        HumanProfile p = HumanProfileAdapter.wrap(profile);
-        assertNull(p.getGivenAndLastNames());
+    public void getDocumentDetails_shouldNotReturnDocumentDetailsForInvalidAttribute() {
+        when(profileMock.getAttribute("document_details")).thenReturn(INVALID_DOCUMENT_DETAILS);
+
+        DocumentDetails result = testObj.getDocumentDetails();
+
+        assertNull(result);
     }
 
     @Test
-    public void getGivenAndLastNamesShouldWorkWithNullFamilyName() {
-        Profile profile = mock(Profile.class);
-        when(profile.getAttribute("given_names")).thenReturn(GIVEN_NAMES);
-        when(profile.getAttribute("family_name")).thenReturn(null);
-        HumanProfile p = HumanProfileAdapter.wrap(profile);
-        assertThat(p.getGivenAndLastNames(), is(GIVEN_NAMES));
+    public void getGivenAndLastNames_getGivenAndLastNamesShouldReturnNullWhenOtherAttributesAreBothNull() {
+        when(profileMock.getAttribute("given_names")).thenReturn(null);
+        when(profileMock.getAttribute("family_name")).thenReturn(null);
+
+        String result = testObj.getGivenAndLastNames();
+
+        assertNull(result);
     }
 
     @Test
-    public void getGivenAndLastNamesShouldWorkWithNullGivenNames() {
-        Profile profile = mock(Profile.class);
-        when(profile.getAttribute("given_names")).thenReturn(null);
-        when(profile.getAttribute("family_name")).thenReturn(FAMILY_NAME);
-        HumanProfile p = HumanProfileAdapter.wrap(profile);
-        assertThat(p.getGivenAndLastNames(), is(FAMILY_NAME));
+    public void getGivenAndLastNames_getGivenAndLastNamesShouldWorkWithNullFamilyName() {
+        when(profileMock.getAttribute("given_names")).thenReturn(GIVEN_NAMES);
+        when(profileMock.getAttribute("family_name")).thenReturn(null);
+
+        String result = testObj.getGivenAndLastNames();
+
+        assertThat(result, is(GIVEN_NAMES));
     }
 
     @Test
-    public void getGivenAndLastNamesShouldWorkWithValidGivenAndFamilyNames() {
-        Profile profile = mock(Profile.class);
-        when(profile.getAttribute("given_names")).thenReturn(GIVEN_NAMES);
-        when(profile.getAttribute("family_name")).thenReturn(FAMILY_NAME);
-        HumanProfile p = HumanProfileAdapter.wrap(profile);
-        assertThat(p.getGivenAndLastNames(), is(GIVEN_NAMES + " " + FAMILY_NAME));
+    public void getGivenAndLastNames_getGivenAndLastNamesShouldWorkWithNullGivenNames() {
+        when(profileMock.getAttribute("given_names")).thenReturn(null);
+        when(profileMock.getAttribute("family_name")).thenReturn(FAMILY_NAME);
+
+        String result = testObj.getGivenAndLastNames();
+
+        assertThat(result, is(FAMILY_NAME));
     }
+
+    @Test
+    public void getGivenAndLastNames_getGivenAndLastNamesShouldWorkWithValidGivenAndFamilyNames() {
+        when(profileMock.getAttribute("given_names")).thenReturn(GIVEN_NAMES);
+        when(profileMock.getAttribute("family_name")).thenReturn(FAMILY_NAME);
+
+        String result = testObj.getGivenAndLastNames();
+
+        assertThat(result, is(GIVEN_NAMES + " " + FAMILY_NAME));
+    }
+
 }
