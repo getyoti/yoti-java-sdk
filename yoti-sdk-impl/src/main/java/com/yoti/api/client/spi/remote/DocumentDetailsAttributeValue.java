79dcb15f507a29601f351a07b3b910b090798485
Michael Buck <michael.buck@yoti.com>
2018-03-08 13:16:02 +0000

    API-407: Support for extended document_details attribute in Java SDK

--- yoti-sdk-impl/src/main/java/com/yoti/api/client/spi/remote/DocumentDetailsAttributeValue.java
+++ yoti-sdk-impl/src/main/java/com/yoti/api/client/spi/remote/DocumentDetailsAttributeValue.java
@@ -1,45 +1,56 @@
 package com.yoti.api.client.spi.remote;
 
-import java.io.UnsupportedEncodingException;
-import java.text.ParseException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import com.yoti.api.client.Date;
 import com.yoti.api.client.DocumentDetails;
 
+import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+
 final class DocumentDetailsAttributeValue implements DocumentDetails {
-    private static final String DETAILS_REGEX = "([A-Za-z]*) ([A-Za-z]{3}) ([A-Za-z0-9]*) (.*)";
-    private static final Pattern DETAILS_PATTERN = Pattern.compile(DETAILS_REGEX);
-    private static final int TYPE_GROUP = 1;
-    private static final int COUNTRY_GROUP = 2;
-    private static final int NUMBER_GROUP = 3;
-    private static final int EXPIRATION_GROUP = 4;
+
+    private static final String MINIMUM_ACCEPTABLE = "([A-Za-z_]*) ([A-Za-z]{3}) ([A-Za-z0-9]{1}).*";
+    private static final int TYPE_INDEX = 0;
+    private static final int COUNTRY_INDEX = 1;
+    private static final int NUMBER_INDEX = 2;
+    private static final int EXPIRATION_INDEX = 3;
+    private static final int AUTHORITY_INDEX = 4;
+
     private final DocumentType type;
     private final String issuingCountry;
     private final Date expirationDate;
     private final String number;
+    private final String authority;
 
-    public DocumentDetailsAttributeValue(DocumentType type, String issuingCountry, Date expirationDate, String number) {
+    public DocumentDetailsAttributeValue(DocumentType type, String issuingCountry, Date expirationDate, String number, String authority) {
         this.type = type;
         this.issuingCountry = issuingCountry;
         this.expirationDate = expirationDate;
         this.number = number;
+        this.authority = authority;
     }
 
     public static DocumentDetails parseFrom(String attribute) throws UnsupportedEncodingException, ParseException {
-        DocumentDetailsAttributeValue result = null;
-        if (attribute != null) {
-            Matcher matcher = DETAILS_PATTERN.matcher(attribute);
-            if (matcher.matches()) {
-                DocumentType type = DocumentType.valueOf(matcher.group(TYPE_GROUP));
-                String issuingCountry = matcher.group(COUNTRY_GROUP);
-                Date expirationDate = DateAttributeValue.parseFrom(matcher.group(EXPIRATION_GROUP));
-                String number = matcher.group(NUMBER_GROUP);
-                result = new DocumentDetailsAttributeValue(type, issuingCountry, expirationDate, number);
-            }
+        if (attribute == null || !attribute.matches(MINIMUM_ACCEPTABLE)) {
+            return null;
         }
-        return result;
+
+        String[] attributes = attribute.split(" ");
+        DocumentType documentType = DocumentType.valueOf(attributes[TYPE_INDEX]);
+        String issuingCountry = attributes[COUNTRY_INDEX];
+        String number = attributes[NUMBER_INDEX];
+        Date expirationDate = getDateSafely(attributes, EXPIRATION_INDEX);
+        String issuingAuthority = getSafely(attributes, AUTHORITY_INDEX);
+        return new DocumentDetailsAttributeValue(documentType, issuingCountry, expirationDate, number, issuingAuthority);
+    }
+
+    private static Date getDateSafely(String[] attributes, int index) throws UnsupportedEncodingException, ParseException {
+        String expirationDate = getSafely(attributes, index);
+        return expirationDate == null ? null : DateAttributeValue.parseFrom(expirationDate);
+    }
+
+    private static String getSafely(String[] attributes, int index) {
+        String value = attributes.length > index ? attributes[index] : null;
+        return "-".equals(value) ? null : value;
     }
 
     @Override
@@ -61,4 +72,21 @@ final class DocumentDetailsAttributeValue implements DocumentDetails {
     public Date getExpirationDate() {
         return expirationDate;
     }
+
+    @Override
+    public String getIssuingAuthority() {
+        return authority;
+    }
+
+    @Override
+    public String toString() {
+        return "DocumentDetailsAttributeValue{" +
+                "type=" + type +
+                ", issuingCountry='" + issuingCountry + '\'' +
+                ", number='" + number + '\'' +
+                ", expirationDate=" + expirationDate +
+                ", issuingAuthority='" + authority + '\'' +
+                '}';
+    }
+
 }
--- yoti-sdk-impl/src/test/java/com/yoti/api/client/spi/remote/DocumentDetailsAttributeValueTest.java
+++ yoti-sdk-impl/src/test/java/com/yoti/api/client/spi/remote/DocumentDetailsAttributeValueTest.java
@@ -1,49 +1,108 @@
 package com.yoti.api.client.spi.remote;
 
+import static com.yoti.api.client.DocumentDetails.DocumentType.AADHAAR;
+import static com.yoti.api.client.DocumentDetails.DocumentType.DRIVING_LICENCE;
+import static com.yoti.api.client.DocumentDetails.DocumentType.PASSPORT;
+import static com.yoti.api.client.DocumentDetails.DocumentType.PASS_CARD;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
-import java.io.UnsupportedEncodingException;
-import java.text.ParseException;
-
+import com.yoti.api.client.DocumentDetails;
 import org.junit.Test;
 
-import com.yoti.api.client.DocumentDetails;
+import java.text.ParseException;
 
 public class DocumentDetailsAttributeValueTest {
-    private static final String VALID_ATTRIBUTE = "PASSPORT GBR 1234abc 2016-05-01";
+
     private static final String INVALID_ATTRIBUTE__TYPE = "XXXPORT GBR 1234abc 2016-05-01";
     private static final String INVALID_ATTRIBUTE__NUMBER = "PASSPORT GBR $%^$%^£ 2016-05-01";
     private static final String INVALID_ATTRIBUTE__COUNTRY = "PASSPORT 13 1234abc 2016-05-01";
-    private static final String INVALID_ATTRIBUTE__DATE = "PASSPORT GBR 1234abc X016-05-01";
+    private static final String INVALID_ATTRIBUTE__DATE = "PASSPORT GBR 1234abc" + " X016-05-01";
 
     @Test
-    public void shouldParseValidAttribute() throws UnsupportedEncodingException, ParseException {
-        DocumentDetails details = DocumentDetailsAttributeValue.parseFrom(VALID_ATTRIBUTE);
-        assertNotNull(details);
-        assertEquals(DocumentDetails.DocumentType.PASSPORT, details.getType());
+    public void shouldReturnNullForNullAttribute() throws Exception {
+        DocumentDetails result = DocumentDetailsAttributeValue.parseFrom(null);
+
+        assertNull(result);
+    }
+
+    @Test
+    public void shouldReturnNullWhenAttributesAreMissing() throws Exception {
+        DocumentDetails result = DocumentDetailsAttributeValue.parseFrom("PASSPORT GBR");
+
+        assertNull(result);
+    }
+
+    @Test
+    public void shouldParseMandatoryAttributes() throws Exception {
+        DocumentDetails result = DocumentDetailsAttributeValue.parseFrom("PASSPORT GBR 1234abc");
+
+        assertNotNull(result);
+        assertEquals(PASSPORT, result.getType());
+        assertEquals("GBR", result.getIssuingCountry());
+        assertEquals("1234abc", result.getDocumentNumber());
+        assertNull(result.getExpirationDate());
+        assertNull(result.getIssuingAuthority());
+    }
+
+    @Test
+    public void shouldParseOneOptionalAttribute() throws Exception {
+        DocumentDetails result = DocumentDetailsAttributeValue.parseFrom("AADHAAR IND 1234abc 2016-05-01");
+
+        assertNotNull(result);
+        assertEquals(AADHAAR, result.getType());
+        assertEquals("IND", result.getIssuingCountry());
+        assertEquals("1234abc", result.getDocumentNumber());
+        assertEquals("2016-05-01", result.getExpirationDate().toString());
+        assertNull(result.getIssuingAuthority());
+    }
+
+    @Test
+    public void shouldParseTwoOptionalAttributes() throws Exception {
+        DocumentDetails result = DocumentDetailsAttributeValue.parseFrom("DRIVING_LICENCE GBR 1234abc 2016-05-01 DVLA");
+
+        assertNotNull(result);
+        assertEquals(DRIVING_LICENCE, result.getType());
+        assertEquals("GBR", result.getIssuingCountry());
+        assertEquals("1234abc", result.getDocumentNumber());
+        assertEquals("2016-05-01", result.getExpirationDate().toString());
+        assertEquals("DVLA", result.getIssuingAuthority());
+    }
+
+    @Test
+    public void shouldParseWhenOneOptionalAttributeIsMissing() throws Exception {
+        DocumentDetails result = DocumentDetailsAttributeValue.parseFrom("PASS_CARD GBR 1234abc - DVLA");
+
+        assertNotNull(result);
+        assertEquals(PASS_CARD, result.getType());
+        assertEquals("GBR", result.getIssuingCountry());
+        assertEquals("1234abc", result.getDocumentNumber());
+        assertNull(result.getExpirationDate());
+        assertEquals("DVLA", result.getIssuingAuthority());
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void shouldFailOnInvalidType() throws UnsupportedEncodingException, ParseException {
+    public void shouldFailOnInvalidType() throws Exception {
         DocumentDetailsAttributeValue.parseFrom(INVALID_ATTRIBUTE__TYPE);
     }
 
     @Test
-    public void shouldFailOnInvalidNumber() throws UnsupportedEncodingException, ParseException {
-        DocumentDetails details = DocumentDetailsAttributeValue.parseFrom(INVALID_ATTRIBUTE__NUMBER);
-        assertNull(details);
+    public void shouldFailOnInvalidNumber() throws Exception {
+        DocumentDetails result = DocumentDetailsAttributeValue.parseFrom(INVALID_ATTRIBUTE__NUMBER);
+
+        assertNull(result);
     }
 
     @Test(expected = ParseException.class)
-    public void shouldFailOnInvalidDate() throws UnsupportedEncodingException, ParseException {
+    public void shouldFailOnInvalidDate() throws Exception {
         DocumentDetailsAttributeValue.parseFrom(INVALID_ATTRIBUTE__DATE);
     }
 
     @Test
-    public void shouldFailOnInvalidCountry() throws UnsupportedEncodingException, ParseException {
-        DocumentDetails details = DocumentDetailsAttributeValue.parseFrom(INVALID_ATTRIBUTE__COUNTRY);
-        assertNull(details);
+    public void shouldFailOnInvalidCountry() throws Exception {
+        DocumentDetails result = DocumentDetailsAttributeValue.parseFrom(INVALID_ATTRIBUTE__COUNTRY);
+
+        assertNull(result);
     }
 }
