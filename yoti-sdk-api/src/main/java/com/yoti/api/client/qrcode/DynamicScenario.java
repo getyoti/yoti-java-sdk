149904fcc031fa5f570dee2026eb4c1da44f33f2
irotech <irotech@gmail.com>
2018-06-08 10:16:13 +0100

    API-335 API-458: javadoc enhancement

--- yoti-sdk-api/src/main/java/com/yoti/api/client/qrcode/DynamicScenario.java
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/qrcode/DynamicScenario.java
@@ -5,22 +5,26 @@ import com.yoti.api.client.qrcode.extension.Extension;
 import com.yoti.api.client.qrcode.policy.Policy;
 
 /**
- * DynamicScenario
+ * Data required for the {@link DynamicQRCode} generation
+ *
  */
 public interface DynamicScenario {
 
     /**
-     * callbackEndpoint
+     * The device's callback endpoint
+     *
      */
     String callbackEndpoint();
 
     /**
-     * policy
+     * The customisable {@link Policy} for the {@link DynamicQRCode}
+     *
      */
     Policy policy();
 
     /**
-     * extensions
+     * Set of {@link Exception} to be activated for the application
+     *
      */
     List<Extension> extensions();
 
