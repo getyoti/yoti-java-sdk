4be970bfa7974c86fa521edcba3bb0db8dcb54b0
michaelb <michael.buck@yoti.com>
2018-11-07 11:25:43 +0000

    SDK-631: Update browser.js to 2.2.0 in example project

--- yoti-sdk-impl/src/main/java/com/yoti/api/client/spi/remote/util/DecryptionHelper.java
+++ yoti-sdk-impl/src/main/java/com/yoti/api/client/spi/remote/util/DecryptionHelper.java
@@ -3,7 +3,6 @@ package com.yoti.api.client.spi.remote.util;
 import static javax.crypto.Cipher.DECRYPT_MODE;
 
 import static com.yoti.api.client.spi.remote.call.YotiConstants.ASYMMETRIC_CIPHER;
-import static com.yoti.api.client.spi.remote.call.YotiConstants.BOUNCY_CASTLE_PROVIDER;
 
 import java.security.GeneralSecurityException;
 import java.security.PrivateKey;
@@ -12,11 +11,13 @@ import javax.crypto.Cipher;
 
 import com.yoti.api.client.ProfileException;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
 public class DecryptionHelper {
 
     public static byte[] decryptAsymmetric(byte[] source, PrivateKey key) throws ProfileException {
         try {
-            Cipher cipher = Cipher.getInstance(ASYMMETRIC_CIPHER, BOUNCY_CASTLE_PROVIDER);
+            Cipher cipher = Cipher.getInstance(ASYMMETRIC_CIPHER, BouncyCastleProvider.PROVIDER_NAME);
             cipher.init(DECRYPT_MODE, key);
             return cipher.doFinal(source);
         } catch (GeneralSecurityException gse) {
