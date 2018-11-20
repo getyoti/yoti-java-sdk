60ec82639d53884db6a11de06310f2a367a65249
michaelb <michael.buck@yoti.com>
2018-11-20 14:44:51 +0000

    SDK-407: Rename - stop referring to Dynamic QR Code everywhere

---
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/qrcode/DynamicShareException.java
@@ -0,0 +1,17 @@
+package com.yoti.api.client.qrcode;
+
+public class DynamicShareException extends Exception {
+
+    public DynamicShareException() {
+        super();
+    }
+
+    public DynamicShareException(String message) {
+        super(message);
+    }
+
+    public DynamicShareException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
