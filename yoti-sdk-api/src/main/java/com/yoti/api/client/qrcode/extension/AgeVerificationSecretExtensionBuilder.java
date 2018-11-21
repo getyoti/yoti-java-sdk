5bf5af30e7cbf05c19d03f77b0436c00338decbc
michaelb <michael.buck@yoti.com>
2018-11-21 17:07:11 +0000

    SDK-407: Drop support for Age Verification Secret

--- yoti-sdk-api/src/main/java/com/yoti/api/client/qrcode/extension/AgeVerificationSecretExtensionBuilder.java
+++
@@ -1,7 +0,0 @@
-package com.yoti.api.client.qrcode.extension;
-
-public interface AgeVerificationSecretExtensionBuilder extends ExtensionBuilder<AgeVerificationSecretContent> {
-
-    AgeVerificationSecretExtensionBuilder withSecretValue(String value);
-
-}
