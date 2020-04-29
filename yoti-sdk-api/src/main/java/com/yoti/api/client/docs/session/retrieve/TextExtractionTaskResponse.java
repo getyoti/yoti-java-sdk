739fc1f4f7685ce027cdf15dd6a6ecbac94fc8cf
Alex Burt <alex.burt@yoti.com>
2020-04-29 15:17:41 +0100

    SDK-1464: Add filtered getters

--- yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/retrieve/TextExtractionTaskResponse.java
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/retrieve/TextExtractionTaskResponse.java
@@ -1,5 +1,9 @@
 package com.yoti.api.client.docs.session.retrieve;
 
+import java.util.List;
+
 public interface TextExtractionTaskResponse extends TaskResponse {
+
+    List<GeneratedTextDataCheckResponse> getGeneratedTextDataChecks();
 
 }
