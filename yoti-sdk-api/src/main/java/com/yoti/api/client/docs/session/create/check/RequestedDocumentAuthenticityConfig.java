93a3400a9935045140192b77cd08464215515e03
Alex Burt <alex.burt@yoti.com>
2020-09-04 15:53:24 +0100

    SDK-1736: Make manual check configurable on requested document authenticity check

--- yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/create/check/RequestedDocumentAuthenticityConfig.java
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/create/check/RequestedDocumentAuthenticityConfig.java
@@ -5,4 +5,12 @@ package com.yoti.api.client.docs.session.create.check;
  */
 public interface RequestedDocumentAuthenticityConfig extends RequestedCheckConfig {
 
+    /**
+     * Returns the value for a manual check for a given
+     * document authenticity check.
+     *
+     * @return the manual check value
+     */
+    String getManualCheck();
+
 }
