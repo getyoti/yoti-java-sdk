c9453d63b0da30313b2c34e14eec220e3677768b
Alex Burt <alex.burt@yoti.com>
2020-09-04 15:53:24 +0100

    SDK-1736: Make manual check configurable on requested document authenticity check

--- yoti-sdk-impl/src/test/java/com/yoti/api/client/docs/session/create/check/SimpleRequestedDocumentAuthenticityCheckBuilderTest.java
+++ yoti-sdk-impl/src/test/java/com/yoti/api/client/docs/session/create/check/SimpleRequestedDocumentAuthenticityCheckBuilderTest.java
@@ -3,6 +3,7 @@ package com.yoti.api.client.docs.session.create.check;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 
 import org.junit.Test;
 
@@ -10,12 +11,40 @@ public class SimpleRequestedDocumentAuthenticityCheckBuilderTest {
 
     @Test
     public void shouldBuildSimpleRequestDocumentAuthenticityCheck() {
-        RequestedCheck result = new SimpleRequestedDocumentAuthenticityCheckBuilder()
+        RequestedDocumentAuthenticityCheck<?> result = new SimpleRequestedDocumentAuthenticityCheckBuilder()
                 .build();
 
         assertThat(result, is(instanceOf(SimpleRequestedDocumentAuthenticityCheck.class)));
         assertThat(result.getConfig(), is(instanceOf(SimpleRequestedDocumentAuthenticityConfig.class)));
         assertThat(result.getType(), is("ID_DOCUMENT_AUTHENTICITY"));
+        assertThat(result.getConfig().getManualCheck(), is(nullValue()));
+    }
+
+    @Test
+    public void withManualCheckAlways_shouldCorrectlySetManualCheckValue() {
+        RequestedDocumentAuthenticityCheck<?> result = new SimpleRequestedDocumentAuthenticityCheckBuilder()
+                .withManualCheckAlways()
+                .build();
+
+        assertThat(result.getConfig().getManualCheck(), is("ALWAYS"));
+    }
+
+    @Test
+    public void withManualCheckFallback_shouldCorrectlySetManualCheckValue() {
+        RequestedDocumentAuthenticityCheck<?> result = new SimpleRequestedDocumentAuthenticityCheckBuilder()
+                .withManualCheckFallback()
+                .build();
+
+        assertThat(result.getConfig().getManualCheck(), is("FALLBACK"));
+    }
+
+    @Test
+    public void withManualCheckNever_shouldCorrectlySetManualCheckValue() {
+        RequestedDocumentAuthenticityCheck<?> result = new SimpleRequestedDocumentAuthenticityCheckBuilder()
+                .withManualCheckNever()
+                .build();
+
+        assertThat(result.getConfig().getManualCheck(), is("NEVER"));
     }
 
 }
