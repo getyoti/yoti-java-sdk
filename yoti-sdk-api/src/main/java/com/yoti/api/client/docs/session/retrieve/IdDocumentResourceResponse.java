54347307e65ce2058553aae5744bdaf869cffa7c
Alex Burt <alex.burt@yoti.com>
2020-10-01 17:07:50 +0100

    SDK-1641: Add support for retrieval of document id photo from an id document

--- yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/retrieve/IdDocumentResourceResponse.java
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/retrieve/IdDocumentResourceResponse.java
@@ -14,4 +14,6 @@ public interface IdDocumentResourceResponse extends ResourceResponse {
 
     List<TextExtractionTaskResponse> getTextExtractionTasks();
 
+    DocumentIdPhotoResponse getDocumentIdPhoto();
+
 }
