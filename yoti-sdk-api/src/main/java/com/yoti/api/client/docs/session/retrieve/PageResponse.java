98d8fff9d71e9075157d801b2a1928fe45fd1845
Alex Burt <alex.burt@yoti.com>
2020-10-05 17:44:17 +0100

    SDK-1774: Allow retrieval of frame for an id document page

--- yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/retrieve/PageResponse.java
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/retrieve/PageResponse.java
@@ -1,9 +1,13 @@
 package com.yoti.api.client.docs.session.retrieve;
 
+import java.util.List;
+
 public interface PageResponse {
 
     String getCaptureMethod();
 
     MediaResponse getMedia();
+
+    List<? extends FrameResponse> getFrames();
 
 }
