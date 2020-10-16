b802ec510400b93bbab6497ca8495b750607aaea
michaelb <michael.buck@yoti.com>
2020-10-16 17:56:08 +0100

    SDK-1712: Private constructor for SandboxDocScanConstants

--- yoti-sdk-sandbox/src/main/java/com/yoti/api/client/sandbox/docs/SandboxDocScanConstants.java
+++ yoti-sdk-sandbox/src/main/java/com/yoti/api/client/sandbox/docs/SandboxDocScanConstants.java
@@ -2,6 +2,8 @@ package com.yoti.api.client.sandbox.docs;
 
 public class SandboxDocScanConstants {
 
+    private SandboxDocScanConstants() { }
+
     public static final String PROGRESS = "PROGRESS";
     public static final String SHOULD_TRY_AGAIN = "SHOULD_TRY_AGAIN";
     public static final String MUST_TRY_AGAIN = "MUST_TRY_AGAIN";
