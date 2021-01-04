8091e1704590bc3ea5cac0910b86230263788401
Alex Burt <alex.burt@yoti.com>
2021-01-04 14:27:58 +0000

    SDK-1861: Consolidate Profile classes and remove the need for adapters

--- yoti-sdk-api/src/main/java/com/yoti/api/client/ApplicationProfile.java
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/ApplicationProfile.java
@@ -1,35 +1,56 @@
 package com.yoti.api.client;
 
-/**
- * Profile of an application with convenience methods to access well-known attributes.
- */
-public interface ApplicationProfile extends Profile {
+import java.util.List;
+
+import com.yoti.api.attributes.AttributeConstants;
+
+public class ApplicationProfile extends Profile {
+
+    /**
+     * Create a new profile based on a list of attributes
+     *
+     * @param attributeList list containing the attributes for this profile
+     */
+    public ApplicationProfile(List<Attribute<?>> attributeList) {
+        super(attributeList);
+    }
 
     /**
      * The name of the application.
+     *
      * @return The name of the application.
      */
-    Attribute<String> getApplicationName();
+    public Attribute<String> getApplicationName() {
+        return getAttribute(AttributeConstants.ApplicationProfileAttributes.ATTRIBUTE_APPLICATION_NAME, String.class);
+    }
 
     /**
      * The URI verified by Portal where the application is available at
+     *
      * @return The URI verified by Portal where the application is available at
      */
-    Attribute<String> getApplicationUrl();
+    public Attribute<String> getApplicationUrl() {
+        return getAttribute(AttributeConstants.ApplicationProfileAttributes.ATTRIBUTE_APPLICATION_URL, String.class);
+    }
 
     /**
      * The logo of the application that will be displayed to those users that perform a sharing with it.
+     *
      * @return The logo of the application that will be displayed to those users that perform a sharing with it.
      */
-    Attribute<Image> getApplicationLogo();
+    public Attribute<Image> getApplicationLogo() {
+        return getAttribute(AttributeConstants.ApplicationProfileAttributes.ATTRIBUTE_APPLICATION_LOGO, Image.class);
+    }
 
     /**
      * The background colour that will be displayed on each receipt the user gets as a result of a sharing with the
      * application.
-     * 
+     *
      * @return The background colour that will be displayed on each receipt the user gets as a result of a sharing with the
      * application.
      */
-    Attribute<String> getApplicationReceiptBgColor();
+    public Attribute<String> getApplicationReceiptBgColor() {
+        return getAttribute(AttributeConstants.ApplicationProfileAttributes.ATTRIBUTE_APPLICATION_RECEIPT_BGCOLOR, String.class);
+    }
 
 }
