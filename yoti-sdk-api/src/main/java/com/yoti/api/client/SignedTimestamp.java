d48205fa6cf2a30fd8f4e9d5433a2c992752691b
Alex Burt <alex.burt@yoti.com>
2021-01-14 14:46:15 +0000

    SDK-1861: Merge SignedTimestampValue into its interface

--- yoti-sdk-api/src/main/java/com/yoti/api/client/SignedTimestamp.java
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/SignedTimestamp.java
@@ -1,24 +1,68 @@
 package com.yoti.api.client;
 
+import com.yoti.api.client.spi.remote.util.Validation;
+
 /**
  *  Represents a point in time, accurate to the nearest microsecond.
  *  SignedTimestamps are cryptographically protected at the time of their creation.
  *
  */
-public interface SignedTimestamp {
+public class SignedTimestamp {
+
+    private final int version;
+    private final DateTime timestamp;
+
+    public SignedTimestamp(int version, DateTime timestamp) {
+        Validation.notNull(timestamp, "timestamp");
+        this.version = version;
+        this.timestamp = timestamp;
+    }
 
     /**
      * Indicates both the version of the protobuf message in use as well as the specific hash algorithms
      *
      * @return The Yoti SignedTimestamp schema this SignedTimestamp conforms to
      */
-    int getVersion();
+    public int getVersion() {
+        return version;
+    }
 
     /**
      * A point in time, to the nearest microsecond
      *
      * @return a point in time, to the nearest microsecond
      */
-    DateTime getTimestamp();
+    public DateTime getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("SignedTimestamp{version=%s, timestamp=%s}", version, timestamp);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        SignedTimestamp that = (SignedTimestamp) o;
+
+        if (version != that.getVersion()) {
+            return false;
+        }
+        return timestamp.equals(that.getTimestamp());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = version;
+        result = 31 * result + (timestamp != null ? timestamp.hashCode() : 0);
+        return result;
+    }
 
 }
