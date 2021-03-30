251e5632ccdf5526ace3debfd9d36aa572c46bc3
michaelb <michael.buck@yoti.com>
2021-03-30 22:25:29 +0100

    SDK-1897: Use a proper builder for the RequestedWatchlistScreeningConfig

--- yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/create/check/RequestedWatchlistScreeningConfig.java
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/create/check/RequestedWatchlistScreeningConfig.java
@@ -1,6 +1,13 @@
 package com.yoti.api.client.docs.session.create.check;
 
+import static com.yoti.api.client.docs.DocScanConstants.ADVERSE_MEDIA;
+import static com.yoti.api.client.docs.DocScanConstants.SANCTIONS;
+import static com.yoti.api.client.spi.remote.util.Validation.notNull;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -15,6 +22,41 @@ public class RequestedWatchlistScreeningConfig implements RequestedCheckConfig {
 
     public List<String> getCategories() {
         return categories;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+
+        private Set<String> categories;
+
+        private Builder() {
+        }
+
+        public Builder withAdverseMediaCategory() {
+            return withCategory(ADVERSE_MEDIA);
+        }
+
+        public Builder withSanctionsCategory() {
+            return withCategory(SANCTIONS);
+        }
+
+        public Builder withCategory(String category) {
+            notNull(category, "category");
+            if (categories == null) {
+                categories = new TreeSet<>();
+            }
+            categories.add(category);
+            return this;
+        }
+
+        public RequestedWatchlistScreeningConfig build() {
+            List<String> categoriesList = categories == null ? null : new ArrayList<>(categories);
+            return new RequestedWatchlistScreeningConfig(categoriesList);
+        }
+
     }
 
 }
