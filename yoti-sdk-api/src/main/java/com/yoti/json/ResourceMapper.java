5195b9f0a2a07cd0ab0c240c11ff8211b0e7b54a
irotech <irotech@gmail.com>
2023-03-07 16:27:46 +0000

    SDK-2231: Add Digital Identity Session creation service

---
+++ yoti-sdk-api/src/main/java/com/yoti/json/ResourceMapper.java
@@ -0,0 +1,39 @@
+package com.yoti.json;
+
+import java.nio.charset.StandardCharsets;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.Nulls;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.cfg.MapperConfig;
+import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
+
+public final class ResourceMapper {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    static {
+        MAPPER.disable(DeserializationFeature.WRAP_EXCEPTIONS)
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .disable(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS)
+                .setVisibility(configureVisibility(MAPPER.getDeserializationConfig()))
+                .setDefaultSetterInfo(JsonSetter.Value.forValueNulls(Nulls.SKIP));
+    }
+
+    private static VisibilityChecker<?> configureVisibility(MapperConfig<?> config) {
+        return config.getDefaultVisibilityChecker()
+                .withFieldVisibility(JsonAutoDetect.Visibility.NONE)
+                .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
+                .withIsGetterVisibility(JsonAutoDetect.Visibility.NONE)
+                .withSetterVisibility(JsonAutoDetect.Visibility.NONE)
+                .withCreatorVisibility(JsonAutoDetect.Visibility.NONE);
+    }
+
+    public static byte[] writeValueAsString(Object object) throws JsonProcessingException {
+        return MAPPER.writeValueAsString(object).getBytes(StandardCharsets.UTF_8);
+    }
+
+}
