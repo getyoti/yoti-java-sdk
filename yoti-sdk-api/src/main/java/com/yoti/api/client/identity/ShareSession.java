8cd27cc6d86151b82a77a7d216a4f5da1fd54648
irotech <irotech@gmail.com>
2023-03-09 10:25:10 +0000

    SDK-2248: Add Digital Identity Session retrieval service

--- yoti-sdk-api/src/main/java/com/yoti/api/client/identity/ShareSession.java
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/identity/ShareSession.java
@@ -1,17 +1,18 @@
 package com.yoti.api.client.identity;
 
+import java.util.Map;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ShareSession {
 
-    @JsonProperty(Property.ID)
     private String id;
-
-    @JsonProperty(Property.STATUS)
     private String status;
-
-    @JsonProperty(Property.EXPIRY)
+    private String created;
+    private String updated;
     private String expiry;
+    private String qrCodeId;
+    private String receiptId;
 
     public String getId() {
         return id;
@@ -21,15 +22,70 @@ public class ShareSession {
         return status;
     }
 
+    public String getCreated() {
+        return created;
+    }
+
+    public String getUpdated() {
+        return updated;
+    }
+
     public String getExpiry() {
         return expiry;
+    }
+
+    public String getQrCodeId() {
+        return qrCodeId;
+    }
+
+    public String getReceiptId() {
+        return receiptId;
+    }
+
+    @JsonProperty(Property.ID)
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @JsonProperty(Property.STATUS)
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @JsonProperty(Property.CREATED)
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    @JsonProperty(Property.UPDATED)
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
+
+    @JsonProperty(Property.EXPIRY)
+    public void setExpiry(String expiry) {
+        this.expiry = expiry;
+    }
+
+    @JsonProperty(Property.QR_CODE)
+    public void setQrCodeId(Map<String, Object> qrCode) {
+        this.qrCodeId = (String) qrCode.get(Property.ID);
+    }
+
+    @JsonProperty(Property.RECEIPT)
+    public void setReceiptId(Map<String, Object> receipt) {
+        this.receiptId = (String) receipt.get(Property.ID);
     }
 
     private static final class Property {
 
         private static final String ID = "id";
+        private static final String CREATED = "created";
+        private static final String UPDATED = "updated";
         private static final String STATUS = "status";
         private static final String EXPIRY = "expiry";
+        private static final String QR_CODE = "qrCode";
+        private static final String RECEIPT = "receipt";
 
     }
 
