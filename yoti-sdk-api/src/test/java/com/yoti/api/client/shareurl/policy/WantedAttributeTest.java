08830b69061c7dd65d94e2322cb48209fd83df1f
irotech <irotech@gmail.com>
2024-03-15 11:33:17 +0000

    SDK-2364: Map attribute alternative names in v1 policy

--- yoti-sdk-api/src/test/java/com/yoti/api/client/shareurl/policy/WantedAttributeTest.java
+++ yoti-sdk-api/src/test/java/com/yoti/api/client/shareurl/policy/WantedAttributeTest.java
@@ -1,20 +1,23 @@
 package com.yoti.api.client.shareurl.policy;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import com.yoti.api.client.shareurl.constraint.Constraint;
 
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.*;
+import org.mockito.junit.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class WantedAttributeTest {
@@ -22,8 +25,7 @@ public class WantedAttributeTest {
     public static final String SOME_NAME = "someName";
     public static final String SOME_DERIVATION = "someDerivation";
 
-    @Mock
-    List<Constraint> constraintListMock;
+    @Mock Constraint constraintMock;
 
     @Test
     public void buildsAnAttribute() {
@@ -31,31 +33,107 @@ public class WantedAttributeTest {
                 .withName(SOME_NAME)
                 .withDerivation(SOME_DERIVATION)
                 .withOptional(true)
-                .build();
-
-        assertEquals(SOME_NAME, result.getName());
-        assertEquals(SOME_DERIVATION, result.getDerivation());
-        assertEquals(true, result.isOptional());
-    }
-
-    @Test
-    public void buildsAnAttributeWithSourceConstraint() {
-        when(constraintListMock.size()).thenReturn(1);
-
-        WantedAttribute result = WantedAttribute.builder()
-                .withName(SOME_NAME)
-                .withDerivation(SOME_DERIVATION)
-                .withOptional(true)
-                .withConstraints(constraintListMock)
                 .withAcceptSelfAsserted(true)
                 .build();
 
         assertEquals(SOME_NAME, result.getName());
         assertEquals(SOME_DERIVATION, result.getDerivation());
-        assertEquals(true, result.isOptional());
-        assertEquals(true, result.getAcceptSelfAsserted());
+        assertTrue(result.isOptional());
+        assertTrue(result.getAcceptSelfAsserted());
+    }
+
+    @Test
+    public void buildsAnAttributeWithSourceConstraints() {
+        List<Constraint> constraints = new ArrayList<>();
+        constraints.add(constraintMock);
+
+        WantedAttribute result = WantedAttribute.builder()
+                .withName(SOME_NAME)
+                .withConstraints(constraints)
+                .build();
+
+        assertEquals(SOME_NAME, result.getName());
         assertThat(result.getConstraints(), hasSize(1));
-        assertEquals(result.getConstraints(), constraintListMock);
+        assertTrue(result.getConstraints().contains(constraintMock));
+    }
+
+    @Test
+    public void buildsAnAttributeWithSourceConstraint() {
+        WantedAttribute result = WantedAttribute.builder()
+                .withName(SOME_NAME)
+                .withConstraint(constraintMock)
+                .build();
+
+        assertEquals(SOME_NAME, result.getName());
+        assertThat(result.getConstraints(), hasSize(1));
+        assertTrue(result.getConstraints().contains(constraintMock));
+    }
+
+    @Test
+    public void buildsAnAttributeWithSingleAndMultipleSourceConstraint() {
+        List<Constraint> constraints = new ArrayList<>();
+        constraints.add(constraintMock);
+
+        Constraint extraConstraintMock = mock(Constraint.class);
+
+        WantedAttribute result = WantedAttribute.builder()
+                .withName(SOME_NAME)
+                .withConstraints(constraints)
+                .withConstraint(extraConstraintMock)
+                .build();
+
+        assertEquals(SOME_NAME, result.getName());
+        assertThat(result.getConstraints(), hasSize(2));
+        assertTrue(result.getConstraints().contains(constraintMock));
+        assertTrue(result.getConstraints().contains(extraConstraintMock));
+    }
+
+    @Test
+    public void buildsAnAttributeWithAlternativeNames() {
+        String anAlternativeName = "anAlternativeName";
+        HashSet<String> alternativeNames = new HashSet<>();
+        alternativeNames.add(anAlternativeName);
+
+        WantedAttribute result = WantedAttribute.builder()
+                .withName(SOME_NAME)
+                .withAlternativeNames(alternativeNames)
+                .build();
+
+        assertEquals(SOME_NAME, result.getName());
+        assertThat(result.getAlternativeNames(), hasSize(1));
+        assertTrue(result.getAlternativeNames().contains(anAlternativeName));
+    }
+
+    @Test
+    public void buildsAnAttributeWithAlternativeName() {
+        String anAlternativeName = "anAlternativeName";
+        WantedAttribute result = WantedAttribute.builder()
+                .withName(SOME_NAME)
+                .withAlternativeName(anAlternativeName)
+                .build();
+
+        assertEquals(SOME_NAME, result.getName());
+        assertThat(result.getAlternativeNames(), hasSize(1));
+        assertTrue(result.getAlternativeNames().contains(anAlternativeName));
+    }
+
+    @Test
+    public void buildsAnAttributeWithSingleAndMultipleAlternativeNames() {
+        String anAlternativeName = "anAlternativeName";
+        String anExtraAlternativeName = "anExtraAlternativeName";
+        HashSet<String> alternativeNames = new HashSet<>();
+        alternativeNames.add(anAlternativeName);
+
+        WantedAttribute result = WantedAttribute.builder()
+                .withName(SOME_NAME)
+                .withAlternativeNames(alternativeNames)
+                .withAlternativeName(anExtraAlternativeName)
+                .build();
+
+        assertEquals(SOME_NAME, result.getName());
+        assertThat(result.getAlternativeNames(), hasSize(2));
+        assertTrue(result.getAlternativeNames().contains(anAlternativeName));
+        assertTrue(result.getAlternativeNames().contains(anExtraAlternativeName));
     }
 
     @Test
