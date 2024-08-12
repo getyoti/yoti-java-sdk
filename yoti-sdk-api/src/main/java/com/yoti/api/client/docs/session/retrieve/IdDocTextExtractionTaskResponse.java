d5da72880b017f7245f15da6162b24ef2f67f15d
Alex Burt <alex.burt@yoti.com>
2024-08-12 14:59:34 +0100

    SDK-2445: [AB] Add text-extraction task recommendation to the GET session endpoint

--- yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/retrieve/IdDocTextExtractionTaskResponse.java
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/retrieve/IdDocTextExtractionTaskResponse.java
@@ -2,7 +2,21 @@ package com.yoti.api.client.docs.session.retrieve;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class IdDocTextExtractionTaskResponse extends TaskResponse {
+
+    @JsonProperty("recommendation")
+    private TaskRecommendationResponse recommendation;
+
+    /**
+     * The recommendation for the task
+     *
+     * @return the recommendation
+     */
+    public TaskRecommendationResponse getRecommendation() {
+        return recommendation;
+    }
 
     public List<GeneratedTextDataCheckResponse> getGeneratedTextDataChecks() {
         return filterGeneratedChecksByType(GeneratedTextDataCheckResponse.class);
--- yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/retrieve/SupplementaryDocumentTextExtractionTaskResponse.java
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/retrieve/SupplementaryDocumentTextExtractionTaskResponse.java
@@ -2,7 +2,21 @@ package com.yoti.api.client.docs.session.retrieve;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class SupplementaryDocumentTextExtractionTaskResponse extends TaskResponse {
+
+    @JsonProperty("recommendation")
+    private TaskRecommendationResponse recommendation;
+
+    /**
+     * The recommendation for the task
+     *
+     * @return the recommendation
+     */
+    public TaskRecommendationResponse getRecommendation() {
+        return recommendation;
+    }
 
     public List<GeneratedSupplementaryDocumentTextDataCheckResponse> getGeneratedTextDataChecks() {
         return filterGeneratedChecksByType(GeneratedSupplementaryDocumentTextDataCheckResponse.class);
