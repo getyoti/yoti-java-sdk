654d91a28b9ff95509c2a625c8c9648477374fcc
Michael Buck <michael.buck@yoti.com>
2024-09-24 14:05:18 +0100

    NA: Consolidate remaining tests in GetSessionResultCheckTest

--- yoti-sdk-api/src/test/java/com/yoti/api/client/docs/session/retrieve/GetSessionResultCheckTest.java
+++ yoti-sdk-api/src/test/java/com/yoti/api/client/docs/session/retrieve/GetSessionResultCheckTest.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import com.yoti.api.client.spi.remote.util.FieldSetter;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -16,6 +15,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GetSessionResultCheckTest {
+
+    GetSessionResult testObj = new GetSessionResult();
 
     @Mock AuthenticityCheckResponse authenticityCheckResponse;
     @Mock FaceMatchCheckResponse faceMatchCheckResponse;
@@ -32,133 +33,40 @@ public class GetSessionResultCheckTest {
     @Mock ProfileDocumentMatchCheckResponse profileDocumentMatchCheckResponse;
     @Mock SynecticsIdentityFraudCheckResponse synecticsIdentityFraudCheckResponse;
 
-    GetSessionResult testObj;
-
-    @Before
-    public void setUp() throws Exception {
-        testObj = new GetSessionResult();
-        setupGetSessionResult();
-    }
-
     @Test
-    public void shouldFilterAuthenticityChecks() {
-        List<AuthenticityCheckResponse> result = testObj.getAuthenticityChecks();
-
-        assertThat(result, contains(authenticityCheckResponse));
-    }
-
-    @Test
-    public void shouldFilterLivenessChecks() {
-        List<LivenessCheckResponse> result = testObj.getLivenessChecks();
-
-        assertThat(result, contains(livenessCheckResponse));
-    }
-
-    @Test
-    public void shouldFilterTextDataChecks() {
-        List<TextDataCheckResponse> result = testObj.getIdDocumentTextDataChecks();
-
-        assertThat(result, contains(textDataCheckResponse));
-    }
-
-    @Test
-    public void shouldFilterSupplementaryDocumentTextDataChecks() {
-        List<SupplementaryDocumentTextDataCheckResponse> result = testObj.getSupplementaryDocumentTextDataChecks();
-
-        assertThat(result, contains(supplementaryDocumentTextDataCheckResponse));
-    }
-
-    @Test
-    public void shouldFilterFaceMatchChecks() {
-        List<FaceMatchCheckResponse> result = testObj.getFaceMatchChecks();
-
-        assertThat(result, contains(faceMatchCheckResponse));
-    }
-
-    @Test
-    public void shouldFilterFaceComparisonChecks() {
-        List<FaceComparisonCheckResponse> result = testObj.getFaceComparisonChecks();
-
-        assertThat(result, contains(faceComparisonCheckResponse));
-    }
-
-    @Test
-    public void shouldFilterIdDocumentComparisonChecks() {
-        List<IdDocumentComparisonCheckResponse> result = testObj.getIdDocumentComparisonChecks();
-
-        assertThat(result, contains(idDocumentComparisonCheckResponse));
-    }
-
-    @Test
-    public void shouldFilterThirdPartyIdentityChecks() {
-        List<ThirdPartyIdentityCheckResponse> result = testObj.getThirdPartyIdentityChecks();
-
-        assertThat(result, contains(thirdPartyIdentityCheckResponse));
-    }
-
-    @Test
-    public void shouldFilterWatchlistScreeningChecks() {
-        List<WatchlistScreeningCheckResponse> result = testObj.getWatchlistScreeningChecks();
-
-        assertThat(result, contains(watchlistScreeningCheckResponse));
-    }
-
-    @Test
-    public void shouldFilterIbvVisualReviewChecks() {
-        List<IbvVisualReviewCheckResponse> result = testObj.getIbvVisualReviewChecks();
-
-        assertThat(result, contains(ibvVisualReviewCheckResponse));
-    }
-
-    @Test
-    public void shouldFilterDocumentSchemeValidityChecks() {
-        List<DocumentSchemeValidityCheckResponse> result = testObj.getDocumentSchemeValidityChecks();
-
-        assertThat(result, contains(documentSchemeValidityCheckResponse));
-    }
-
-    @Test
-    public void shouldFilterProfileDocumentMatchChecks() {
-        List<ProfileDocumentMatchCheckResponse> result = testObj.getProfileDocumentMatchChecks();
-
-        assertThat(result, contains(profileDocumentMatchCheckResponse));
-    }
-
-    @Test
-    public void shouldFilterThirdPartyIdentityFraudOneCheck() {
-        List<ThirdPartyIdentityFraudOneCheckResponse> result = testObj.getThirdPartyIdentityFraudOneChecks();
-
-        assertThat(result, contains(thirdPartyIdentityFraudOneCheckResponse));
-    }
-
-    @Test
-    public void shouldFilterSynecticsIdentityFraudCheck() {
-        List<SynecticsIdentityFraudCheckResponse> result = testObj.getSynecticsIdentityFraudChecks();
-
-        assertThat(result, contains(synecticsIdentityFraudCheckResponse));
-    }
-
-    private void setupGetSessionResult() {
-        FieldSetter.setField(
-                testObj,
-                "checks",
-                Arrays.asList(
-                        authenticityCheckResponse,
-                        livenessCheckResponse,
-                        textDataCheckResponse,
-                        supplementaryDocumentTextDataCheckResponse,
-                        faceMatchCheckResponse,
-                        faceComparisonCheckResponse,
-                        idDocumentComparisonCheckResponse,
-                        thirdPartyIdentityCheckResponse,
-                        watchlistScreeningCheckResponse,
-                        thirdPartyIdentityFraudOneCheckResponse,
-                        ibvVisualReviewCheckResponse,
-                        documentSchemeValidityCheckResponse,
-                        profileDocumentMatchCheckResponse,
-                        synecticsIdentityFraudCheckResponse
-                )
+    public void shouldFilterChecks() {
+        List<CheckResponse> allChecks = Arrays.asList(
+                authenticityCheckResponse,
+                livenessCheckResponse,
+                textDataCheckResponse,
+                supplementaryDocumentTextDataCheckResponse,
+                faceMatchCheckResponse,
+                faceComparisonCheckResponse,
+                idDocumentComparisonCheckResponse,
+                thirdPartyIdentityCheckResponse,
+                watchlistScreeningCheckResponse,
+                thirdPartyIdentityFraudOneCheckResponse,
+                ibvVisualReviewCheckResponse,
+                documentSchemeValidityCheckResponse,
+                profileDocumentMatchCheckResponse,
+                synecticsIdentityFraudCheckResponse
         );
+        FieldSetter.setField(testObj, "checks", allChecks);
+
+        assertThat(testObj.getAuthenticityChecks(), contains(authenticityCheckResponse));
+        assertThat(testObj.getLivenessChecks(), contains(livenessCheckResponse));
+        assertThat(testObj.getIdDocumentTextDataChecks(), contains(textDataCheckResponse));
+        assertThat(testObj.getSupplementaryDocumentTextDataChecks(), contains(supplementaryDocumentTextDataCheckResponse));
+        assertThat(testObj.getFaceMatchChecks(), contains(faceMatchCheckResponse));
+        assertThat(testObj.getFaceComparisonChecks(), contains(faceComparisonCheckResponse));
+        assertThat(testObj.getIdDocumentComparisonChecks(), contains(idDocumentComparisonCheckResponse));
+        assertThat(testObj.getThirdPartyIdentityChecks(), contains(thirdPartyIdentityCheckResponse));
+        assertThat(testObj.getWatchlistScreeningChecks(), contains(watchlistScreeningCheckResponse));
+        assertThat(testObj.getIbvVisualReviewChecks(), contains(ibvVisualReviewCheckResponse));
+        assertThat(testObj.getDocumentSchemeValidityChecks(), contains(documentSchemeValidityCheckResponse));
+        assertThat(testObj.getProfileDocumentMatchChecks(), contains(profileDocumentMatchCheckResponse));
+        assertThat(testObj.getThirdPartyIdentityFraudOneChecks(), contains(thirdPartyIdentityFraudOneCheckResponse));
+        assertThat(testObj.getSynecticsIdentityFraudChecks(), contains(synecticsIdentityFraudCheckResponse));
     }
 
 }
