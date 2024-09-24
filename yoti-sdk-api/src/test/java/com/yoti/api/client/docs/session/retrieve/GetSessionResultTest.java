5123a0b4ff763fa2e67d47ee7ab695f4fb70157d
Michael Buck <michael.buck@yoti.com>
2024-09-24 11:01:42 +0100

    SDK-2528: Helper method for finding the Resources that belong to a Check

--- yoti-sdk-api/src/test/java/com/yoti/api/client/docs/session/retrieve/GetSessionResultTest.java
+++ yoti-sdk-api/src/test/java/com/yoti/api/client/docs/session/retrieve/GetSessionResultTest.java
@@ -2,21 +2,29 @@ package com.yoti.api.client.docs.session.retrieve;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import com.yoti.api.client.spi.remote.util.FieldSetter;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GetSessionResultTest {
 
-    GetSessionResult testObj = new GetSessionResult();
+    private static final String AUTH_CHECK_ID = "authCheckId";
+
+    @InjectMocks GetSessionResult testObj = new GetSessionResult();
 
     @Mock AuthenticityCheckResponse authenticityCheckResponseMock;
     @Mock FaceMatchCheckResponse faceMatchCheckResponseMock;
@@ -32,6 +40,14 @@ public class GetSessionResultTest {
     @Mock DocumentSchemeValidityCheckResponse documentSchemeValidityCheckResponseMock;
     @Mock ProfileDocumentMatchCheckResponse profileDocumentMatchCheckResponseMock;
     @Mock SynecticsIdentityFraudCheckResponse synecticsIdentityFraudCheckResponseMock;
+
+    @Mock(name = "resources") ResourceContainer resourceContainerMock;
+    @Mock ResourceContainer checkResourceContainerMock;
+
+    @Before
+    public void setUp() throws Exception {
+        when(authenticityCheckResponseMock.getId()).thenReturn(AUTH_CHECK_ID);
+    }
 
     @Test
     public void shouldFilterChecks() {
@@ -67,6 +83,23 @@ public class GetSessionResultTest {
         assertThat(testObj.getProfileDocumentMatchChecks(), contains(profileDocumentMatchCheckResponseMock));
         assertThat(testObj.getThirdPartyIdentityFraudOneChecks(), contains(thirdPartyIdentityFraudOneCheckResponseMock));
         assertThat(testObj.getSynecticsIdentityFraudChecks(), contains(synecticsIdentityFraudCheckResponseMock));
+    }
+
+    @Test
+    public void getResourcesForCheck_shouldThrowExceptionForBadCheckId() {
+        FieldSetter.setField(testObj, "checks", Collections.singletonList(authenticityCheckResponseMock));
+
+        assertThrows(IllegalArgumentException.class, () -> testObj.getResourcesForCheck("someBadCheckId"));
+    }
+
+    @Test
+    public void getResourcesForCheck_shouldReturnResultFromResources() {
+        FieldSetter.setField(testObj, "checks", Collections.singletonList(authenticityCheckResponseMock));
+        when(resourceContainerMock.filterForCheck(authenticityCheckResponseMock)).thenReturn(checkResourceContainerMock);
+
+        ResourceContainer result = testObj.getResourcesForCheck(AUTH_CHECK_ID);
+
+        assertThat(result, is(checkResourceContainerMock));
     }
 
 }
--- yoti-sdk-api/src/test/java/com/yoti/api/client/docs/session/retrieve/ResourceContainerTest.java
+++ yoti-sdk-api/src/test/java/com/yoti/api/client/docs/session/retrieve/ResourceContainerTest.java
@@ -1,15 +1,20 @@
 package com.yoti.api.client.docs.session.retrieve;
 
+import static java.util.Arrays.asList;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.emptyIterable;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import com.yoti.api.client.spi.remote.util.FieldSetter;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -18,24 +23,64 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class ResourceContainerTest {
 
+    private static final String ID_DOC_1_ID = "idDocumentResource1Id";
+    private static final String SUPPLEMENTARY_DOC_1_ID = "supplementaryDocResource1Id";
+    private static final String ZOOM_RESOURCE_1_ID = "zoomLivenessResource1MockId";
+    private static final String FACE_CAPTURE_1_ID = "faceCaptureResource1Id";
+    private static final String PROFILE_1_ID = "applicantProfileResource1Id";
+
     ResourceContainer testObj = new ResourceContainer();
 
-    @Mock ZoomLivenessResourceResponse zoomLivenessResourceMock;
+    @Mock IdDocumentResourceResponse idDocResourceResponse1Mock;
+    @Mock IdDocumentResourceResponse idDocResourceResponse2Mock;
+    @Mock SupplementaryDocumentResourceResponse supplementaryDocResourceResponse1Mock;
+    @Mock SupplementaryDocumentResourceResponse supplementaryDocResourceResponse2Mock;
+    @Mock ZoomLivenessResourceResponse zoomLivenessResource1Mock;
+    @Mock ZoomLivenessResourceResponse zoomLivenessResource2Mock;
     @Mock StaticLivenessResourceResponse staticLivenessResourceMock;
     @Mock LivenessResourceResponse livenessResource;
+    @Mock FaceCaptureResourceResponse faceCaptureResourceResponse1Mock;
+    @Mock FaceCaptureResourceResponse faceCaptureResourceResponse2Mock;
+    @Mock ApplicantProfileResourceResponse applicantProfileResourceResponse1Mock;
+    @Mock ApplicantProfileResourceResponse applicantProfileResourceResponse2Mock;
 
-    @Test
-    public void shouldFilterZoomLivenessResources() {
-        FieldSetter.setField(testObj, "livenessCapture", Arrays.asList(zoomLivenessResourceMock, staticLivenessResourceMock, livenessResource));
+    @Mock CheckResponse checkResponseMock;
 
-        List<ZoomLivenessResourceResponse> result = testObj.getZoomLivenessResources();
-
-        assertThat(result, contains(zoomLivenessResourceMock));
+    @Before
+    public void setUp() throws Exception {
+        when(idDocResourceResponse1Mock.getId()).thenReturn(ID_DOC_1_ID);
+        when(idDocResourceResponse2Mock.getId()).thenReturn("idDocumentResource2Id");
+        when(supplementaryDocResourceResponse1Mock.getId()).thenReturn(SUPPLEMENTARY_DOC_1_ID);
+        when(supplementaryDocResourceResponse2Mock.getId()).thenReturn("supplementaryDocResource2Id");
+        when(zoomLivenessResource1Mock.getId()).thenReturn(ZOOM_RESOURCE_1_ID);
+        when(zoomLivenessResource2Mock.getId()).thenReturn("zoomLivenessResource2MockId");
+        when(faceCaptureResourceResponse1Mock.getId()).thenReturn(FACE_CAPTURE_1_ID);
+        when(faceCaptureResourceResponse2Mock.getId()).thenReturn("faceCaptureResource2Id");
+        when(applicantProfileResourceResponse1Mock.getId()).thenReturn(PROFILE_1_ID);
+        when(applicantProfileResourceResponse2Mock.getId()).thenReturn("applicantProfileResource2Id");
     }
 
     @Test
-    public void shouldFilterStaticLivenessResources() {
-        FieldSetter.setField(testObj, "livenessCapture", Arrays.asList(zoomLivenessResourceMock, staticLivenessResourceMock, livenessResource));
+    public void getZoomLivenessResources_shouldFilterZoomLivenessResources() {
+        FieldSetter.setField(testObj, "livenessCapture", asList(zoomLivenessResource1Mock, staticLivenessResourceMock, livenessResource));
+
+        List<ZoomLivenessResourceResponse> result = testObj.getZoomLivenessResources();
+
+        assertThat(result, contains(zoomLivenessResource1Mock));
+    }
+
+    @Test
+    public void getZoomLivenessResources_shouldReturnEmptyList() {
+        FieldSetter.setField(testObj, "livenessCapture", new ArrayList<>());
+
+        List<ZoomLivenessResourceResponse> result = testObj.getZoomLivenessResources();
+
+        assertThat(result, hasSize(0));
+    }
+
+    @Test
+    public void getStaticLivenessResources_shouldFilterStaticLivenessResources() {
+        FieldSetter.setField(testObj, "livenessCapture", asList(zoomLivenessResource1Mock, staticLivenessResourceMock, livenessResource));
 
         List<StaticLivenessResourceResponse> result = testObj.getStaticLivenessResources();
 
@@ -43,12 +88,58 @@ public class ResourceContainerTest {
     }
 
     @Test
-    public void shouldReturnEmptyList() {
+    public void getStaticLivenessResources_shouldReturnEmptyList() {
         FieldSetter.setField(testObj, "livenessCapture", new ArrayList<>());
 
-        List<ZoomLivenessResourceResponse> result = testObj.getZoomLivenessResources();
+        List<StaticLivenessResourceResponse> result = testObj.getStaticLivenessResources();
 
         assertThat(result, hasSize(0));
+    }
+
+    @Test
+    public void filterForCheck_shouldHandleNullResources() {
+        ResourceContainer result = testObj.filterForCheck(checkResponseMock);
+
+        assertThat(result.getIdDocuments(), is(emptyIterable()));
+        assertThat(result.getSupplementaryDocuments(), is(emptyIterable()));
+        assertThat(result.getLivenessCapture(), is(emptyIterable()));
+        assertThat(result.getFaceCapture(), is(emptyIterable()));
+        assertThat(result.getApplicantProfiles(), is(emptyIterable()));
+    }
+
+    @Test
+    public void filterForCheck_shouldReturnEmptyWhenNoMatches() {
+        FieldSetter.setField(testObj, "idDocuments", new ArrayList<>());
+        FieldSetter.setField(testObj, "supplementaryDocuments", new ArrayList<>());
+        FieldSetter.setField(testObj, "livenessCapture", new ArrayList<>());
+        FieldSetter.setField(testObj, "faceCapture", new ArrayList<>());
+        FieldSetter.setField(testObj, "applicantProfiles", new ArrayList<>());
+
+        ResourceContainer result = testObj.filterForCheck(checkResponseMock);
+
+        assertThat(result.getIdDocuments(), is(emptyIterable()));
+        assertThat(result.getSupplementaryDocuments(), is(emptyIterable()));
+        assertThat(result.getLivenessCapture(), is(emptyIterable()));
+        assertThat(result.getFaceCapture(), is(emptyIterable()));
+        assertThat(result.getApplicantProfiles(), is(emptyIterable()));
+    }
+
+    @Test
+    public void filterForCheck_shouldReturnFilteredCollections() {
+        FieldSetter.setField(testObj, "idDocuments", asList(idDocResourceResponse1Mock, idDocResourceResponse2Mock));
+        FieldSetter.setField(testObj, "supplementaryDocuments", asList(supplementaryDocResourceResponse1Mock, supplementaryDocResourceResponse2Mock));
+        FieldSetter.setField(testObj, "livenessCapture", asList(zoomLivenessResource1Mock, zoomLivenessResource2Mock, staticLivenessResourceMock));
+        FieldSetter.setField(testObj, "faceCapture", asList(faceCaptureResourceResponse1Mock, faceCaptureResourceResponse2Mock));
+        FieldSetter.setField(testObj, "applicantProfiles", asList(applicantProfileResourceResponse1Mock, applicantProfileResourceResponse2Mock));
+        when(checkResponseMock.getResourcesUsed()).thenReturn(asList(ID_DOC_1_ID, SUPPLEMENTARY_DOC_1_ID, ZOOM_RESOURCE_1_ID, FACE_CAPTURE_1_ID, PROFILE_1_ID));
+
+        ResourceContainer result = testObj.filterForCheck(checkResponseMock);
+
+        assertThat(result.getIdDocuments(), contains(idDocResourceResponse1Mock));
+        assertThat(result.getSupplementaryDocuments(), contains(supplementaryDocResourceResponse1Mock));
+        assertThat(result.getLivenessCapture(), contains(zoomLivenessResource1Mock));
+        assertThat(result.getFaceCapture(), contains(faceCaptureResourceResponse1Mock));
+        assertThat(result.getApplicantProfiles(), contains(applicantProfileResourceResponse1Mock));
     }
 
 }
