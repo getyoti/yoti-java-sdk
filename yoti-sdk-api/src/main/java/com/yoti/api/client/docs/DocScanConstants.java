1c07134b22f9d765292f9286eccb0404c2c43d3e
Michael Buck <michael.buck@yoti.com>
2024-09-25 14:28:03 +0100

    SDK-2309: [MB] The biometric_consent_flow is EARLY not EAGER

--- yoti-sdk-api/src/main/java/com/yoti/api/client/docs/DocScanConstants.java
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/docs/DocScanConstants.java
@@ -87,7 +87,7 @@ public class DocScanConstants {
 
     public static final String GBP = "GBP";
 
-    public static final String EAGER = "EAGER";
+    public static final String EARLY = "EARLY";
     public static final String JUST_IN_TIME = "JUST_IN_TIME";
 
     public static final String ON = "ON";
