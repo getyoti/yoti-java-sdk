0cc28eab90885448d4e3406156df4a35b9c93426
Alex Burt <alex.burt@yoti.com>
2024-09-27 15:25:48 +0100

    SDK-2533: Change IdentityProfileResponse to use a stronly typed object for the report

---
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/retrieve/IdentityProfileSchemeResponse.java
@@ -0,0 +1,31 @@
+package com.yoti.api.client.docs.session.retrieve;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class IdentityProfileSchemeResponse {
+
+    @JsonProperty("type")
+    private String type;
+
+    @JsonProperty("objective")
+    private String objective;
+
+    /**
+     * The type of the scheme
+     *
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * The objective of the scheme
+     *
+     * @return the objective
+     */
+    public String getObjective() {
+        return objective;
+    }
+
+}
