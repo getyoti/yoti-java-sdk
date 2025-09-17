9f7e365025cadc390433a83b2b7b72eb507a4b19
Alex Burt <alex.burt@yoti.com>
2025-09-18 00:38:51 +0100

    SDK-2675: Add support for configuring check result, for FACE_COMPARISON check in the sandbox

---
+++ yoti-sdk-sandbox/src/main/java/com/yoti/api/client/sandbox/docs/request/check/SandboxFaceComparisonCheck.java
@@ -0,0 +1,40 @@
+package com.yoti.api.client.sandbox.docs.request.check;
+
+import static com.yoti.api.client.spi.remote.util.Validation.notNull;
+
+import com.yoti.api.client.sandbox.docs.request.check.report.SandboxCheckReport;
+
+public class SandboxFaceComparisonCheck extends SandboxCheck {
+
+    SandboxFaceComparisonCheck(SandboxCheckResult result) {
+        super(result);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * Builder for {@link SandboxFaceComparisonCheck}
+     */
+    public static class Builder extends SandboxCheck.Builder<Builder> {
+
+        private Builder() {}
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+
+        @Override
+        public SandboxFaceComparisonCheck build() {
+            notNull(recommendation, "recommendation");
+
+            SandboxCheckReport report = new SandboxCheckReport(recommendation, breakdown);
+            SandboxCheckResult result = new SandboxCheckResult(report, reportTemplate);
+
+            return new SandboxFaceComparisonCheck(result);
+        }
+
+    }
+}
