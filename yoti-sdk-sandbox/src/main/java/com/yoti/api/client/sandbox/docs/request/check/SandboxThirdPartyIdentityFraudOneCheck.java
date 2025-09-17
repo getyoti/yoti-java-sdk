42e700a2c2a24748cde839c133a710db9b9f2384
Alex Burt <alex.burt@yoti.com>
2025-09-18 00:50:33 +0100

    SDK-2675: Add support for configuring check result, for THIRD_PARTY_IDENTITY_FRAUD_1 check in the sandbox

---
+++ yoti-sdk-sandbox/src/main/java/com/yoti/api/client/sandbox/docs/request/check/SandboxThirdPartyIdentityFraudOneCheck.java
@@ -0,0 +1,40 @@
+package com.yoti.api.client.sandbox.docs.request.check;
+
+import static com.yoti.api.client.spi.remote.util.Validation.notNull;
+
+import com.yoti.api.client.sandbox.docs.request.check.report.SandboxCheckReport;
+
+public class SandboxThirdPartyIdentityFraudOneCheck extends SandboxCheck {
+
+    SandboxThirdPartyIdentityFraudOneCheck(SandboxCheckResult result) {
+        super(result);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * Builder for {@link SandboxThirdPartyIdentityFraudOneCheck}
+     */
+    public static class Builder extends SandboxCheck.Builder<Builder> {
+
+        private Builder() {}
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+
+        @Override
+        public SandboxThirdPartyIdentityFraudOneCheck build() {
+            notNull(recommendation, "recommendation");
+
+            SandboxCheckReport report = new SandboxCheckReport(recommendation, breakdown);
+            SandboxCheckResult result = new SandboxCheckResult(report, reportTemplate);
+
+            return new SandboxThirdPartyIdentityFraudOneCheck(result);
+        }
+
+    }
+}
