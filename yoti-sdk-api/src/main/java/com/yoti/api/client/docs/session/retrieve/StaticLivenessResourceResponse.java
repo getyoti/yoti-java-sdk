2adf6307fc62d6531f7b421bb67d8731eb44d19f
Alex Burt <alex.burt@yoti.com>
2026-01-06 10:48:56 +0000

    SDK-2779: Expose capture_type property on StaticLivenessResourceResponse

--- yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/retrieve/StaticLivenessResourceResponse.java
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/retrieve/StaticLivenessResourceResponse.java
@@ -7,8 +7,15 @@ public class StaticLivenessResourceResponse extends LivenessResourceResponse {
     @JsonProperty("image")
     private ImageResponse image;
 
+    @JsonProperty("capture_type")
+    private String captureType;
+
     public ImageResponse getImage() {
         return image;
+    }
+
+    public String getCaptureType() {
+        return captureType;
     }
 
 }
