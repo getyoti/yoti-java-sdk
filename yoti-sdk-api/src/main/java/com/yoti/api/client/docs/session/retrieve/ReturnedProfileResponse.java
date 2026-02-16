608c926d726a5e17292f2a581322095573563df5
Alex Burt <alex.burt@yoti.com>
2026-02-16 10:56:15 +0000

    SDK-2784: Use MediaResponse class instead of Media interface for ShareCode response classes

--- yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/retrieve/ReturnedProfileResponse.java
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/docs/session/retrieve/ReturnedProfileResponse.java
@@ -1,16 +1,14 @@
 package com.yoti.api.client.docs.session.retrieve;
 
-import com.yoti.api.client.Media;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ReturnedProfileResponse {
 
     @JsonProperty("media")
-    private Media media;
+    private MediaResponse media;
 
-    public Media getMedia() {
+    public MediaResponse getMedia() {
         return media;
     }
-    
+
 }
