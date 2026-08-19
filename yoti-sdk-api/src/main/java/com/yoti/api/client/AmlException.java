0a28fcc96cbe7fd95fb31207ac1a3376e782614d
Alex Burt <alex.burt@yoti.com>
2026-08-19 11:17:57 +0100

    SDK-2800: Deprecate discontinued AML check API ahead of removal in next major

--- yoti-sdk-api/src/main/java/com/yoti/api/client/AmlException.java
+++ yoti-sdk-api/src/main/java/com/yoti/api/client/AmlException.java
@@ -2,7 +2,11 @@ package com.yoti.api.client;
 
 /**
  * Signals that a problem occurred while performing an AML check
+ *
+ * @deprecated The AML check service has been discontinued and this class is no longer used.
+ *             This API will be removed in the next major release.
  */
+@Deprecated
 public class AmlException extends Exception {
 
     private static final long serialVersionUID = 3210133542178934016L;
